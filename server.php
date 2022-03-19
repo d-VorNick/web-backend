41580ce4c893906763800806c22848bb20b7a4be
d-VorNick <vnikita2000@gmail.com>
2022-03-19 06:13:49 +0300

    Labs

--- server.php
+++ server.php
@@ -3,7 +3,7 @@ use Workerman\Worker;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$wsWorker = new Worker('websocket://web-2sem.herokuapp.com');
+$wsWorker = new Worker('websocket://0.0.0.0');
 $wsWorker->count = 1;
 
 
