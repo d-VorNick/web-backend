ea12361df49832e4e7e3256a64bbfc82d9d6c5be
d-VorNick <vnikita2000@gmail.com>
2022-03-19 05:08:16 +0300

    Labs

--- web/js/game.js
+++ web/js/game.js
@@ -1,7 +1,7 @@
 const bodyElement = document.getElementById('wrapper');
 const unit = document.getElementById('unit');
 
-const ws = new WebSocket('wss://web-2sem.herokuapp.com:2346');
+const ws = new WebSocket('wss://localhost');
 
 
 document.addEventListener('keyup', event => {
